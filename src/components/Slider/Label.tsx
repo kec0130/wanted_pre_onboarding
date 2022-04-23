da6afac8af9aaa2c0038775ee45d690b674c874c
Eunchae Ko <kec0130@gmail.com>
2022-04-23 14:45:46 +0900

    Update slider label

--- src/components/Slider/Label.tsx
+++ src/components/Slider/Label.tsx
@@ -2,35 +2,49 @@ import "./Label.css";
 
 interface LabelProps {
   step: number;
-  setInputValue: React.Dispatch<React.SetStateAction<string>>;
+  inputValue: number;
+  setInputValue: React.Dispatch<React.SetStateAction<number>>;
   // eslint-disable-next-line no-unused-vars
-  handleProgress: (value: string) => void;
+  handleProgress: (value: number) => void;
 }
 
 export default function Label({
   step,
+  inputValue,
   setInputValue,
   handleProgress,
 }: LabelProps) {
-  const labelList = Array(100 / step + 1)
+  const labelCount = 100 / step + 1;
+  const labelList = Array(labelCount)
     .fill(0)
     .map((_, i) => i * step);
 
   return (
     <div className="label-wrap">
-      {labelList.map((label) => (
-        <button
-          key={label}
-          type="button"
-          className="label"
-          onClick={() => {
-            setInputValue(label.toString());
-            handleProgress(label.toString());
-          }}
-        >
-          {label}%
-        </button>
-      ))}
+      <div className="label-circle-wrap">
+        {labelList.map((label) => (
+          <span
+            className={`label-circle ${
+              label <= inputValue ? "label-circle--active" : ""
+            }`}
+          />
+        ))}
+      </div>
+      <div className="label-button-wrap">
+        {labelList.map((label) => (
+          <button
+            key={label}
+            type="button"
+            className="label-button"
+            onClick={() => {
+              setInputValue(label);
+              handleProgress(label);
+            }}
+          >
+            {label}%
+          </button>
+        ))}
+      </div>
     </div>
   );
 }
--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -1,13 +1,22 @@
 import { useState } from "react";
+import Label from "./Label";
 import "./index.css";
 
 export default function Slider() {
-  const [inputValue, setInputValue] = useState("0");
+  const [inputValue, setInputValue] = useState(0);
 
-  const handleProgress = (value: string) => {
-    const slider = document.querySelector<HTMLInputElement>(".slider");
+  const handleProgress = (value: number) => {
+    const slider = document.querySelector<HTMLInputElement>(".slider__input");
     if (!slider) return;
-    slider.style.background = `linear-gradient(to right, var(--highlight-color) 0%, var(--highlight-color) ${value}%, var(--gray-3) ${value}%, var(--gray-3) 100%)`;
+    slider.style.background = `
+      linear-gradient(
+        to right,
+        var(--highlight-color) 0%,
+        var(--highlight-color) ${value}%,
+        var(--bg-gray-main) ${value}%,
+        var(--bg-gray-main) 100%
+      )
+    `;
   };
 
   return (
@@ -17,18 +26,28 @@ export default function Slider() {
           <span className="value">{inputValue}</span>
           <span>%</span>
         </div>
-        <input
-          type="range"
-          min="0"
-          max="100"
-          className="slider"
-          value={inputValue}
-          onChange={(event) => {
-            const { value } = event.target;
-            setInputValue(value);
-            handleProgress(value);
-          }}
-        />
+        <div>
+          <div className="slider__input-wrap">
+            <input
+              type="range"
+              min="0"
+              max="100"
+              className="slider__input"
+              value={inputValue}
+              onChange={(event) => {
+                const { value } = event.target;
+                setInputValue(Number(value));
+                handleProgress(Number(value));
+              }}
+            />
+          </div>
+          <Label
+            step={25}
+            inputValue={inputValue}
+            setInputValue={setInputValue}
+            handleProgress={handleProgress}
+          />
+        </div>
       </div>
     </article>
   );
