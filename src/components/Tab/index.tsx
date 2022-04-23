d7fea4188bb9b57881c1adcbd55c58b0c54a6c3c
Eunchae Ko <kec0130@gmail.com>
2022-04-23 15:54:46 +0900

    Fix tab highlighting

--- src/components/Tab/index.tsx
+++ src/components/Tab/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { CSSProperties, Fragment, useState } from "react";
 import "./index.css";
 
 interface TabProps {
@@ -7,20 +7,32 @@ interface TabProps {
 
 export default function Tab({ tabs }: TabProps) {
   const [currentTab, setCurrentTab] = useState(tabs[0]);
+  const tabWidth: CSSProperties = { width: `${100 / tabs.length}%` };
 
   return (
     <article className="container">
+      <h3 className="title">Tab</h3>
       <div className="tabs">
-        {tabs.map((tab) => (
-          <button
-            type="button"
-            className={`tab ${currentTab === tab ? "tab--active" : ""}`}
-            key={tab}
-            onClick={() => setCurrentTab(tab)}
-            style={{ width: `${100 / tabs.length}%` }}
-          >
-            {tab}
-          </button>
+        {tabs.map((tab, index) => (
+          <Fragment key={tab}>
+            <button
+              type="button"
+              className={`tab ${currentTab === tab ? "tab--active" : ""}`}
+              onClick={() => setCurrentTab(tab)}
+              style={tabWidth}
+            >
+              {tab}
+            </button>
+            {currentTab === tab && (
+              <span
+                className="tab__highlight"
+                style={{
+                  ...tabWidth,
+                  left: `${(100 / tabs.length) * index}%`,
+                }}
+              />
+            )}
+          </Fragment>
         ))}
       </div>
     </article>
